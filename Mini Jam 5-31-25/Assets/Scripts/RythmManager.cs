bd17b5ac734acc934242913582915ac3b687d25e
Maximilian Kinney <maxekinney@yahoo.com>
2025-05-31 14:12:18 +0200

    basic rythm code

---
+++ Mini Jam 5-31-25/Assets/Scripts/RythmManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+
+public class RythmManager : MonoBehaviour
+{
+    public bool walking = true;
+
+    public UnityEvent onCanHit;
+    public UnityEvent onCantHit;
+    public enum RythmType
+    {
+        None,
+        Beat,
+        Bar,
+        Measure,
+        Phrase
+    }
+    public float timeToHit = 0.5f;
+    public float[] intervals; //interval between first and second step
+
+    public AudioSource stepSound;
+
+    private void Start()
+    {
+        StartCoroutine(WalkCycle());
+    }
+
+    //hit logic
+    private bool canHit = false;
+    private IEnumerator WalkCycle()
+    {
+        while (walking)
+        {
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                // Wait for the specified interval
+                yield return new WaitForSeconds(intervals[i]);
+                // Trigger the step action
+                Step();
+
+                onCanHit?.Invoke();
+                canHit = true;
+                Debug.Log("can hit!");
+                yield return new WaitForSeconds(timeToHit);
+
+                onCantHit?.Invoke();
+                Debug.Log("Can't hit!");
+                canHit = false;
+                
+
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canHit)
+        {
+            // Trigger hit logic
+            Debug.Log("Hit!");
+        }
+    }
+   
+    private void Step()
+    {
+        stepSound.Play();
+    }
+}
